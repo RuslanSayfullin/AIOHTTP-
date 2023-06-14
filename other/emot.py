1d1d68b881dd298575d69914719bed20dbbf4e24
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-06-15 00:09:15 +0500

    Библиотека Emot предназначена для распознавания эмодзи в тексте.

---
+++ other/emot.py
@@ -0,0 +1,9 @@
+import emot
+
+text = "Привет! Как дела? \U0001F600 Хорошо, спасибо! Ж-)"
+
+emoji = emot.emoji(text)
+print(emoji_info)
+
+emoticon_info = emot.emoticons(text)
+print(emoticon_info)
